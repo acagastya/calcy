c13dfbcd4651c1ad29484e8b77ea6aaa93bf01c1
Varun <varuna246@gmail.com>
2018-02-08 15:04:21 +0530

    pwa

--- sw.js
+++ sw.js
@@ -1,14 +1,11 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-//Install stage sets up the offline page in the cahche and opens a new cache
 self.addEventListener('install', function (event) {
     event.waitUntil(preLoad());
 });
 
 var preLoad = function () {
-    console.log('[PWA Builder] Install Event processing');
-    return caches.open('pwabuilder-offline').then(function (cache) {
-        console.log('[PWA Builder] Cached index and offline page during Install');
+    console.log('Install Event processing');
+    return caches.open('calcypwa').then(function (cache) {
+        console.log('Cached index and offline page during Install');
         return cache.addAll(['/offline.html', '/index.html']);
     });
 }
@@ -35,16 +32,16 @@ var checkResponse = function (request) {
 };
 
 var addToCache = function (request) {
-    return caches.open('pwabuilder-offline').then(function (cache) {
+    return caches.open('calcypwa').then(function (cache) {
         return fetch(request).then(function (response) {
-            console.log('[PWA Builder] add page to offline' + response.url)
+            console.log('add page to offline' + response.url)
             return cache.put(request, response);
         });
     });
 };
 
 var returnFromCache = function (request) {
-    return caches.open('pwabuilder-offline').then(function (cache) {
+    return caches.open('calcypwa').then(function (cache) {
         return cache.match(request).then(function (matching) {
             if (!matching || matching.status == 404) {
                 return cache.match('offline.html')
